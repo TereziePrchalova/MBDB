a4355ca05d7ceae257f31cab48cf324cf10b603c
Terezie <terezie.prchalova1@gmail.com>
2024-05-23 09:11:33 +0200

    Added MeasurementTab

---
+++ src/components/mst/mstTabs/MeasurementTab.js
@@ -0,0 +1,39 @@
+import FormWrapper from "../../buildingBlocks/FormWrapper";
+import ArrayField from "../../buildingBlocks/ArrayField";
+import Measurement from "../measurement/Measurement";
+import UseDefault from "../../buildingBlocks/UseDefault";
+
+function MeasurementTab( { name } ) {
+
+    const fieldName = 'measurements'
+
+    UseDefault(`${name}.${fieldName}`, [{}] );
+
+    return (
+      <>
+        <div className="-mt-3">
+            <ArrayField
+                name={name}
+                label="Measurement"
+                required={true}
+                fieldName={fieldName}
+                tooltip='List of the information about each measurement. This includes target(s), ligand(s), chemical environment, and the position of the sample within the instrument'
+                renderChild={({ arrayName, index }) => (
+                    <FormWrapper
+                        headline={`Measurement ${index + 1}`}
+                        tooltip='List of the information about each measurement. This includes target(s), ligand(s), chemical environment, and the position of the sample within the instrument'
+                    >
+                        <div>
+                            <Measurement
+                                name={`${arrayName}.${index}`}
+                            />
+                        </div>
+                    </FormWrapper>
+                )}
+            />
+        </div>
+      </>
+    );
+}
+
+export default MeasurementTab;
