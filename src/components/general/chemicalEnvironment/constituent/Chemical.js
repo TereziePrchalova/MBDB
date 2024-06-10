bb37edfe786a08cf637de055b55f48c77237d776
Terezie <terezie.prchalova1@gmail.com>
2024-06-10 12:52:06 +0200

    Concentration added

--- src/components/general/chemicalEnvironment/constituent/Chemical.js
+++ src/components/general/chemicalEnvironment/constituent/Chemical.js
@@ -1,5 +1,6 @@
 import ArrayField from "../../../buildingBlocks/ArrayField";
 import CustomField from "../../../buildingBlocks/CustomField";
+import Concentration from "../../../sharedComponents/Concentration";
 
 function Chemical( { name } ) {
 
@@ -15,6 +16,12 @@ function Chemical( { name } ) {
           width='w-full'
         />
       </div>
+      <div className="mb-3">
+        <Concentration
+          name={`${name}.concentration`}
+          tooltip='Concentration of the constituent including its relative concentration related to the collected sample or absolute concentration of the constituent'
+        />
+      </div>
       <div className='flex'>
           <div className='mr-3'>
             <CustomField
@@ -25,7 +32,7 @@ function Chemical( { name } ) {
               tooltip='The basic information about the chemical. This information is pulled from Pubchem, however if the information is not available it can be manually filled'
             />
           </div>
-          <div className="-mt-3 mr-8">
+          <div className="-mt-3">
               <ArrayField
                   name={name}
                   label='Additional specification'
